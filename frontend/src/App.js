45f6382f675af112a282a001b5d02a14284884f2
Chris Ramshaw <chris.ramshaw@gmail.com>
2018-07-03 09:59:51 +0100

    add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { NavLink } from 'react-router-dom'
+import Home from './components/home';
+import EnsIds from './components/EnsIds';
+import EnsId from './components/EnsId';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+      <div>
+        <NavLink to="/genes">Genes</NavLink>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/genes" component={EnsIds}/>
+        <Route path="/genes/:EnsId" component={EnsId}/>
       </div>
+      </Router>
     );
   }
 }
